2388c7d331b5848f995918b77b4753b39bc2d4a3
pdxtrader <benjaminblodgett@gmail.com>
2017-04-16 21:31:30 -0600

    testing new approaches

---
+++ src/jets/JetDriver.java
@@ -0,0 +1,61 @@
+package jets;
+
+public class JetDriver {
+	public static void main(String[] args) {
+		
+		int menu[][][] =  //declaration and instantiation
+		{	{ {1, 2, 3}, {4, 5, 6}, {7, 8, 9} },
+			{ {10, 11, 12}, {13, 14, 15}, {16, 17, 18} },
+			{ {19, 20, 21}, {22, 23, 24}, {25, 26, 27} } };
+			
+		
+		
+		String jetNames[]= new String[x]; //declaration and instantiation
+			jetNames[0] = "Jet1";
+			jetNames[1] = "Jet2";
+			jetNames[2] = "Jet3";
+			jetNames[3] = "Jet4";
+			jetNames[4] = "Jet5";
+			
+			System.out.println(jetNames[0]);
+			System.out.println(jetNames[1]);
+			System.out.println(jetNames[2]);
+			System.out.println(jetNames[3]);
+			System.out.println(jetNames[4]);
+	
+//			i need to clare it first
+			
+//		for ( int i = 0 ; i > jetNames.length; i++){
+			jetNames[jetNames.length + (1)] = "Jet6";
+//		}
+//			just add an if null modifier and store a bunch of nulls
+		
+			System.out.println(jetNames[0]);
+			System.out.println(jetNames[1]);
+			System.out.println(jetNames[2]);
+			System.out.println(jetNames[3]);
+			System.out.println(jetNames[4]);
+			System.out.println(jetNames[5]);
+		
+//			to array length then add with array assignment plus array length t
+//			interaate to array.length then
+//			add new array vairable to the 
+//			array.length +1 (or ++)
+//			(might have to save arraylength+1 as a var)
+//			
+			
+			//initialization
+//			menu[1] = 20;
+//			menu[2] = 30;
+//			menu[3] = 40;
+//			menu[4] = 50;
+	
+		
+//		to add one it interates array length + 1
+		
+		
+//		for ( i = 0; jets.equals(obj)
+			
+		
+	}
+}
---
+++ src/jets/MachCalc.java
@@ -0,0 +1,10 @@
+package jets;
+
+public class MachCalc {
+	int mph;
+	int mach;
+	
+	void machCalc(int mph){
+		double calcMach = mph*0.00130332;
+	}
+}
